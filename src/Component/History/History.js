749b1447badac299ad400dcd0eb907c2a597c38f
jihyeon <jihyun@wjtb.net>
2022-04-07 22:26:44 +0900

    지현과제 > 위에 식=답 까지 옮기는 건 됬으나, 연달아서 추가가 되지 않음ㅠ

---
+++ src/Component/History/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const History = (props) => {
+    return (
+        <div className='history'>
+            <span>{props.data}</span>
+        </div>
+    );
+};
+
+export default History;
--- src/Page/Calculator/Calculator.js
+++ src/Page/Calculator/Calculator.js
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Total from '../../Component/Total'
 import Btn from '../../Component/Btn'
+import History from '../../Component/History';
 
 const Calculator = () => {
     let check = /^[0-9]+$/; 
     const keys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','%','='];
-    const [num, setNum] = useState(0)
+    const [num, setNum] = useState(0);
+    const [form, setForm] = useState('');
+    const [history, putHistory] = useState({
+        idx : '',
+        txt: ''
+    });
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        putHistory({
+          ...history,
+          [name]: value
+        });
+    };
+    const nextId = useRef(1);
+
+    const reSetNum = () => {
+        setNum(0);
+    }
     const inNumber = no => {
-        if(num[num.length-1] == no && !check.test(no))return
+        if(num[num.length-1] == no && !check.test(no)) return
         else (num == '0')?setNum(no):setNum(num+no)
         if(no == '='){
-            setNum(eval(num));
+            // setNum(eval(num));
+            nextId.current += 1;
+            setForm(num + '=' + eval(num));
+            reSetNum();
         }
     }
+
     return (
         <div className='Wrapper'>
-            <Total rs={num}/>
+            <History data={form}/>
+            <Total rs={num} onChange={onChange}/>
             <div className='buttonWrapper'>
                 {keys.map((str, index) => <Btn key={index} val={str} callBack={inNumber}/>)}
             </div>
